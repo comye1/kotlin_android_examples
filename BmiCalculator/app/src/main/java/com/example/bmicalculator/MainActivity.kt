6e185c4847ac3f11b472d68c08c92782fbc232fe
comye1 <50735594+comye1@users.noreply.github.com>
2021-01-02 12:05:46 +0900

    resultActivity로 전환하기, Anko 라이브러리 코드 사용

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.bmicalculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultButton.setOnClickListener {
+            // 인텐트 객체를 생성하고 startActivity() 호출
+            //val intent = Intent(this, ResultActivity::class.java)
+            //startActivity(intent)
+
+
+            //anko 라이브러리
+            startActivity<ResultActivity>()
+
+        }
     }
 }
